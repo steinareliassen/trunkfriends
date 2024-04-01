ee94a23bcc824fb13b74ea1cae8114575cc90ddf
SteinarEliassen <steinar.eliassen@gmail.com>
2024-04-01 10:52:46 +0200

    Fix issue with refreshing history

--- src/main/kotlin/org/osprey/trunkfriends/handlers/refreshHistory.kt
+++ src/main/kotlin/org/osprey/trunkfriends/handlers/refreshHistory.kt
@@ -18,23 +18,24 @@ suspend fun refreshHistory(selectedConfig : Pair<String, Config>, isCancelled : 
         val followers = hostInterface.getFollow(userId, Direction.FOLLOWERS, isCancelled, feedbackFunction)
 
         // Get current users
-        val currentUsers =
-            mutableMapOf<String, CurrentUser>().also { currentUsers ->
+        val currentUsers = mutableMapOf<String, CurrentUser>()
 
-                followers.forEach {
-                    currentUsers[it.acct] =
-                        CurrentUser(following = false, follower = true, it.acct, it.username)
-                }
+        followers.forEach {
+            currentUsers[it.acct] =
+                CurrentUser(following = false, follower = true, it.acct, it.username)
+        }
 
-                following.forEach {
-                    currentUsers[it.acct].let { currentUser ->
-                        currentUser?.copy(
-                            following = true
-                        ) ?: CurrentUser(following = true, follower = false, it.acct, it.username)
-                    }
-                }
-
+        following.forEach {
+            if (currentUsers.containsKey(it.acct)) {
+                currentUsers[it.acct] = currentUsers[it.acct]?.copy(
+                    following = true
+                ) ?: throw IllegalStateException("Should not be possible")
+            } else {
+                currentUsers[it.acct] =
+                    CurrentUser(following = true, follower = false, it.acct, it.username
+                    )
             }
+        }
 
         // Find user status from previous run
         val history = HistoryData(selectedConfig.first) // Read old history from file. History can contain multiple entries pr user
